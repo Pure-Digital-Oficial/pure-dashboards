fd3f5fb52f261042ed62fe02eb2d1bb051d0e7b8
erick.empresa <erickcelestimo@gmail.com>
2025-03-17 16:12:42 -0300

    refactor: create modules from filters

--- src/feature/pages/time_notes_dashboard.py
+++ src/feature/pages/time_notes_dashboard.py
@@ -39,58 +39,60 @@ class TimeNotesDashboard:
         self.data['Data'] = pd.to_datetime(self.data['Data'], format='%d/%m/%Y')
         self.data['AnoMes'] = self.data['Data'].dt.to_period('M')
 
-    def renderPage(self):
-        st.title('APONTAMENTOS HORAS TRABALHADAS')
-        self.normalizeData()
-        filterData = self.data['Data']
-        
-        # Filters Data
+    def setupFilters(self):
+        st.sidebar.title('Filtros')
+        self.filterData = self.data['Data']
         names = self.data['Nome'].unique().tolist()
         modalities = self.data['Modalidade'].unique().tolist()
         projects = self.data['Projeto'].str.upper().unique().tolist()        
-        months = filterData.dt.month.sort_values(ascending=True).unique().tolist()
+        months = self.filterData.dt.month.sort_values(ascending=True).unique().tolist()
         months = [self.monthPtBr[month] for month in months]
-        years = filterData.dt.year.sort_values(ascending=True).unique().tolist()
+        years = self.filterData.dt.year.sort_values(ascending=True).unique().tolist()
+        
+        self.selected_name = st.sidebar.selectbox('Equipe', ['Todos'] + names)
+        self.selected_modality = st.sidebar.selectbox('Modalidades', ['Todos'] + modalities)
+        self.selected_year = st.sidebar.selectbox('Ano', ['Todos'] + years)
+        self.selected_month = st.sidebar.selectbox('Mês', ['Todos'] + months)
+        self.selected_project = st.sidebar.selectbox('Projetos', ['Todos'] + projects)
 
-        # Filters
-        st.sidebar.title('Filtros')
-        selected_name = st.sidebar.selectbox('Equipe', ['Todos'] + names)
-        selected_modality = st.sidebar.selectbox('Modalidades', ['Todos'] + modalities)
-        selected_year = st.sidebar.selectbox('Ano', ['Todos'] + years)
-        selected_month = st.sidebar.selectbox('Mês', ['Todos'] + months)
-        selected_project = st.sidebar.selectbox('Projetos', ['Todos'] + projects)
-
+    def applyFilters(self):
         data_filter = TimeNotesDataFilter(self.data)
-        filtered_data = data_filter \
-            .filter_by_name(selected_name) \
-            .filter_by_modality(selected_modality) \
-            .filter_by_year(selected_year, filterData) \
-            .filter_by_month(selected_month, filterData, self.monthPtBr) \
-            .filter_by_project(selected_project) \
+        self.filtered_data = data_filter \
+            .filter_by_name(self.selected_name) \
+            .filter_by_modality(self.selected_modality) \
+            .filter_by_year(self.selected_year, self.filterData) \
+            .filter_by_month(self.selected_month, self.filterData, self.monthPtBr) \
+            .filter_by_project(self.selected_project) \
             .get_filtered_data()
 
-        # Tables
-        sumHoursFromTeamMember = filtered_data.groupby('Nome')[['Horas']].sum()
-        sumHoursFromFeature = filtered_data.groupby('Modalidade', as_index=False)[['Horas']].sum()
-        sumHoursFromProject = filtered_data.groupby('Projeto', as_index=False)[['Horas']].sum()
+    def renderPage(self):
+        st.title('APONTAMENTOS HORAS TRABALHADAS')
+        self.normalizeData()
+        self.setupFilters()
+        self.applyFilters()
         
-        quantityHoursFromTeamMember = filtered_data.groupby('Nome')[['Horas']].count()
-        quantityHoursFromFeature = filtered_data.groupby('Modalidade', as_index=False)[['Horas']].count()
-        quantityHoursFromProject = filtered_data.groupby('Projeto', as_index=False)[['Horas']].count()
+        # Tables
+        sumHoursFromTeamMember = self.filtered_data.groupby('Nome')[['Horas']].sum()
+        sumHoursFromFeature = self.filtered_data.groupby('Modalidade', as_index=False)[['Horas']].sum()
+        sumHoursFromProject = self.filtered_data.groupby('Projeto', as_index=False)[['Horas']].sum()
+        
+        quantityHoursFromTeamMember = self.filtered_data.groupby('Nome')[['Horas']].count()
+        quantityHoursFromFeature = self.filtered_data.groupby('Modalidade', as_index=False)[['Horas']].count()
+        quantityHoursFromProject = self.filtered_data.groupby('Projeto', as_index=False)[['Horas']].count()
 
-        sumHoursFromMonth = filtered_data.groupby('AnoMes', as_index=False)[['Horas']].sum()
+        sumHoursFromMonth = self.filtered_data.groupby('AnoMes', as_index=False)[['Horas']].sum()
         sumHoursFromMonth['Ano'] = sumHoursFromMonth['AnoMes'].dt.year
         sumHoursFromMonth['Mes'] = sumHoursFromMonth['AnoMes'].dt.month
         sumHoursFromMonth['Mes'] = sumHoursFromMonth['Mes'].map(self.monthPtBr)
         sumHoursFromMonth = sumHoursFromMonth.drop(columns=['AnoMes'])
 
-        quantityHoursFromMonth = filtered_data.groupby('AnoMes', as_index=False)[['Horas']].count()
+        quantityHoursFromMonth = self.filtered_data.groupby('AnoMes', as_index=False)[['Horas']].count()
         quantityHoursFromMonth['Ano'] = quantityHoursFromMonth['AnoMes'].dt.year
         quantityHoursFromMonth['Mes'] = quantityHoursFromMonth['AnoMes'].dt.month
         quantityHoursFromMonth['Mes'] = quantityHoursFromMonth['Mes'].map(self.monthPtBr)
         quantityHoursFromMonth = quantityHoursFromMonth.drop(columns=['AnoMes'])
 
-        details = filtered_data[['Nome', 'Modalidade', 'Feature', 'Observacao', 'Problemas', 'Duvidas']]
+        details = self.filtered_data[['Nome', 'Modalidade', 'Feature', 'Observacao', 'Problemas', 'Duvidas']]
         details.set_index("Nome", inplace=True)
 
         # Graphs
@@ -164,24 +166,24 @@ class TimeNotesDashboard:
         with tabHours:
             columnLeft, columnRight = st.columns(2)
             with columnLeft:
-                st.metric('Soma Horas Apontadas', pd.to_numeric(filtered_data['Horas']).sum())
+                st.metric('Soma Horas Apontadas', pd.to_numeric(self.filtered_data['Horas']).sum())
                 st.plotly_chart(fig_sum_hours_team, use_container_width = True)
                 st.plotly_chart(fig_sum_hours_feature, use_container_width = True)
             
             with columnRight:
-                st.metric('Qtd de Apontamentos', filtered_data['Horas'].count())
+                st.metric('Qtd de Apontamentos', self.filtered_data['Horas'].count())
                 st.plotly_chart(fig_sum_hours_month, use_container_width = True)
                 st.plotly_chart(fig_sum_hours_project, use_container_width = True)
 
         with tabQuantity:
                 columnLeft, columnRight = st.columns(2)
                 with columnLeft:
-                    st.metric('Soma Horas Apontadas', pd.to_numeric(filtered_data['Horas']).sum())
+                    st.metric('Soma Horas Apontadas', pd.to_numeric(self.filtered_data['Horas']).sum())
                     st.plotly_chart(fig_quantity_hours_team, use_container_width = True)
                     st.plotly_chart(fig_quantity_hours_feature, use_container_width = True)
 
                 with columnRight:
-                    st.metric('Qtd de Apontamentos', filtered_data['Horas'].count())
+                    st.metric('Qtd de Apontamentos', self.filtered_data['Horas'].count())
                     st.plotly_chart(fig_quantity_hours_month, use_container_width = True)
                     st.plotly_chart(fig_quantity_hours_project, use_container_width = True)
         
