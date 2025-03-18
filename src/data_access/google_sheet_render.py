7edc63a6c0a5a0019ce3633421fcc05c3a0ef968
Erick da Silva Celestino <99321670+ErickCelestino@users.noreply.github.com>
2025-03-18 09:00:46 -0300

    Configure service account enviroment variable (#4)
    * chore: configure service account from enviroment variable (#3)

--- src/data_access/google_sheet_render.py
+++ src/data_access/google_sheet_render.py
@@ -2,13 +2,30 @@ import os
 import gspread
 import pandas as pd
 from dotenv import load_dotenv
-#src\data_access\google_sheet_render.py
+import json
+import base64
+
 class GoogleSheetsReader:
-    def __init__(self, service_account_file="service-account.json"):
+    def __init__(self):
         load_dotenv(dotenv_path=".env")
         self.spreadsheet_url = os.getenv("GOOGLE_SHEETS_URL")
         self.worksheet_name = os.getenv('GOOGLE_SHEETS_TIME_NOTES_WORKSHEET')
-        self.credentials = gspread.service_account(filename=service_account_file)
+        service_account_info = os.getenv("GOOGLE_SERVICE_ACCOUNT")
+        
+        self.service_account_info = json.loads(base64.b64decode(service_account_info).decode())
+        self.credentials = gspread.service_account_from_dict(self.service_account_info)
+
+    def formalize_data(self):
+        self.data['Nome'] = self.data['Nome'].str.strip()
+        self.data['Nome'] = self.data['Nome'].str.upper()
+        self.data['Modalidade'] = self.data['Modalidade'].str.strip()
+        self.data['Modalidade'] = self.data['Modalidade'].str.upper()
+        self.data['Horas'] = self.data['Horas'].str.replace(',', '.')
+        self.data['Horas'] = pd.to_numeric(self.data['Horas'])
+        self.data['Data'] = self.data['Data'].str.strip()
+        self.data['Data'] = pd.to_datetime(self.data['Data'], format='%d/%m/%Y')
+        self.data['AnoMes'] = self.data['Data'].dt.to_period('M')
+        self.data['Projeto'] = self.data['Projeto'].str.upper()
 
     def formalize_data(self):
         self.data['Nome'] = self.data['Nome'].str.strip()
